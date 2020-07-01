2fc4cf538da064a92c7fc436c1652ee91b6a8acb
whykatherine <whykatherine@gmail.com>
2020-07-02 01:07:30 +0800

    feat: export image from canvas

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// words
+let text = document.getElementById("text");
 let poem = document.getElementById("poem");
 let words = document.getElementsByClassName("word");
 for (let word of words) {
@@ -11,7 +13,24 @@ for (let word of words) {
   });
 }
 
+// clear
 let clear = document.getElementById("clear");
-clear.addEventListener("click", (event) => {
+clear.addEventListener("click", () => {
   poem.innerHTML = "";
+});
+
+// export
+let exportBtn = document.getElementById("export");
+exportBtn.addEventListener("click", () => {
+  html2canvas(text, {
+    logging: false
+  }).then(canvas => {
+    let imageURI = canvas.toDataURL("image/png");
+    // https://stackoverflow.com/a/37673039
+    let anchor = document.createElement("a");
+    anchor.href = imageURI;
+    anchor.target = "_blank";
+    anchor.download = "poem.png";
+    anchor.click();
+  });
 });
