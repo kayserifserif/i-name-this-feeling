891d71bc2d1d0634a1609f86625fe7f82b3ba208
Katherine Yang <whykatherine@gmail.com>
2023-07-06 17:10:41 -0400

    feat: style overhaul

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,65 @@
-let text = document.getElementById("text");
-let poem = document.getElementById("poem");
-let words = document.getElementsByClassName("word");
-let endingControls = document.getElementById("ending_controls");
-endingControls.reset();
+const NUM_INIT_WORDS = 3;
 
-// randomise
-function shuffle(elems) { // https://j11y.io/javascript/shuffling-the-dom/
-  allElems = (function(){
-    var ret = [], l = elems.length;
-    while (l--) { ret[ret.length] = elems[l]; }
-    return ret;
-  })();
+const text = document.querySelector(".text");
+const poem = document.querySelector(".poem");
+const endingControls = document.getElementById("ending-controls");
 
-  var shuffled = (function(){
-    var l = allElems.length, ret = [];
-    while (l--) {
-      var random = Math.floor(Math.random() * allElems.length),
-          randEl = allElems[random].cloneNode(true);
-      allElems.splice(random, 1);
-      ret[ret.length] = randEl;
+const WORD_TEMPLATE = document.querySelector(".word");
+WORD_TEMPLATE.remove();
+const WORDS_CONTAINER = document.querySelector("#choose-controls");
+fetch("words.txt")
+  .then(r => r.text())
+  .then(text => {
+    // create list of words
+    const words = text.trim().split("\n");
+    shuffle(words);
+
+    // create and add elements
+    words.forEach(word => {
+      const el = WORD_TEMPLATE.cloneNode(true);
+      el.innerText = word;
+      el.addEventListener("click", () => {
+        addNewFeeling(word);
+      });
+      WORDS_CONTAINER.appendChild(el);
+    });
+
+    // initialise first few
+    for (let i = 0; i < NUM_INIT_WORDS; i++) {
+      const word = words[i];
+      addNewFeeling(word);
     }
-    return ret; 
-  })(), l = elems.length;
+  });
 
-  while (l--) {
-    elems[l].parentNode.insertBefore(shuffled[l], elems[l].nextSibling);
-    elems[l].parentNode.removeChild(elems[l]);
+// https://javascript.info/task/shuffle
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
-shuffle(words);
 
-// initialise first three
-for (let i = 0; i < 3; i++) {
-  let pos = Math.floor(Math.random() * words.length);
-  let word = words[pos];
-  addNewFeeling(word.textContent);
-}
-
-// words
-for (let word of words) {
-  word.addEventListener("click", (event) => {
-    addNewFeeling(event.target.textContent);
-  });
-}
 function newBreak() {
-  let newBreak = document.createElement("span");
+  const newBreak = document.createElement("span");
   newBreak.textContent = " ";
   newBreak.classList.add("break");
   newBreak.addEventListener("click", createBreak);
   return newBreak;
 }
+
 function scrollToBottom() {
   window.scrollTo({
     top: document.body.scrollHeight,
     behavior: "smooth"
   });
 }
+
 function addNewFeeling(text) {
   let para = null;
   if (poem.children.length == 0) {
@@ -68,7 +72,8 @@ function addNewFeeling(text) {
     }
     para.appendChild(newBreak());
   }
-  let newFeeling = document.createElement("span");
+
+  const newFeeling = document.createElement("span");
   newFeeling.textContent = text;
   newFeeling.classList.add("feeling");
   para.appendChild(newFeeling);
@@ -79,66 +84,64 @@ function addNewFeeling(text) {
   if (poem.children.length == 0) {
     poem.appendChild(para);
   }
+
   scrollToBottom();
 }
 
 // handle
-let handleBtn = document.getElementById("handle");
+const handleBtn = document.getElementById("handle");
 let isExpanded = true;
-let handleImg = handleBtn.firstChild;
 handleBtn.addEventListener("click", (event) => {
-  let toolbox = document.getElementById("toolbox");
-  if (isExpanded) {
-    toolbox.style.height = "80px";
-    handleImg.src = "img/up-arrow.svg";
-  } else {
-    toolbox.style.height = "auto";
-    handleImg.src = "img/down-arrow.svg";
-  }
+  const toolbox = document.querySelector(".toolbox");
   isExpanded = !isExpanded;
+  toolbox.classList.toggle("expanded", isExpanded);
+  const handleImg = handleBtn.querySelector("img");
+  handleImg.src = isExpanded ? "img/down-arrow.svg" : "img/up-arrow.svg";
 });
 
 // break
-let breakBtn = document.getElementById("insertBreak");
-let breaks = document.getElementsByClassName("break");
+const breakBtn = document.getElementById("insertBreak");
 breakBtn.addEventListener("click", () => {
-  for (let b of breaks) {
+  const breaks = document.querySelectorAll(".break");
+  for (const b of breaks) {
     b.classList.add("break-active");
   }
 });
 function createBreak(event) {
-  let b = event.target;
-  let els = [];
+  const b = event.target;
+  const els = [];
   let el = b.nextSibling;
   while (el) {
     els.push(el);
     el = el.nextSibling;
   }
-  let para = document.createElement("p");
-  for (let el of els) {
+  const para = document.createElement("p");
+  for (const el of els) {
     para.appendChild(el);
   }
   poem.appendChild(para);
-  for (let b1 of breaks) {
+
+  const breaks = document.querySelectorAll(".break");
+  for (const b1 of breaks) {
     b1.classList.remove("break-active");
   }
 }
 
 // clear
-let clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", (event) => {
+const clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () => {
   poem.innerHTML = "";
 });
 
 // download
-let downloadBtn = document.getElementById("download");
-downloadBtn.addEventListener("click", (event) => {
+const downloadBtn = document.getElementById("download");
+downloadBtn.addEventListener("click", () => {
   html2canvas(text, {
     logging: false
   }).then(canvas => {
-    let imageURI = canvas.toDataURL("image/png");
+    const imageURI = canvas.toDataURL("image/png");
     // https://stackoverflow.com/a/37673039
-    let anchor = document.createElement("a");
+    const anchor = document.createElement("a");
     anchor.href = imageURI;
     anchor.target = "_blank";
     anchor.download = "poem.png";
@@ -147,26 +150,22 @@ downloadBtn.addEventListener("click", (event) => {
 });
 
 // reverse
-let reverseBtn = document.getElementById("reverse");
+const reverseBtn = document.getElementById("reverse");
 let isLightMode = true;
 reverseBtn.addEventListener("click", (event) => {
   var themeLink = document.getElementsByTagName("link")[1];
-  if (isLightMode) {
-    themeLink.href = "css/dark.css";
-  } else {
-    themeLink.href = "css/light.css";
-  }
   isLightMode = !isLightMode;
+  themeLink.href = isLightMode ? "css/light.css" : "css/dark.css";
 });
 
 // font size control
-let sizeControls = document.getElementsByClassName("sizeControl");
-let title = document.getElementById("title");
-for (let sizeControl of sizeControls) {
+const sizeControls = document.getElementsByClassName("sizeControl");
+const title = document.querySelector(".title");
+for (const sizeControl of sizeControls) {
   sizeControl.addEventListener("click", (event) => {
-    let titleOrig = parseInt(getComputedStyle(title).fontSize);
-    let poemOrig = parseInt(getComputedStyle(poem).fontSize);
-    let mult = (sizeControl.id === "bigger") ? 1.1 : 0.9;
+    const titleOrig = parseInt(getComputedStyle(title).fontSize);
+    const poemOrig = parseInt(getComputedStyle(poem).fontSize);
+    const mult = (sizeControl.id === "bigger") ? 1.1 : 0.9;
     title.style.fontSize = (titleOrig * mult) + "px";
     poem.style.fontSize = (poemOrig * mult) + "px";
   });
@@ -175,13 +174,13 @@ for (let sizeControl of sizeControls) {
 // trailing and
 endingControls.addEventListener("change", (event) => {
   if (poem.children.length > 0) {
-    let val = event.target.value;
-    let para = poem.lastChild;
+    const val = event.target.value;
+    const para = poem.lastChild;
     if (val === "yesTrailingAnd") {
       para.appendChild(newBreak());
       para.appendChild(document.createTextNode("and"));
     } else {
-      let b = para.querySelector(".break:last-of-type");
+      const b = para.querySelector(".break:last-of-type");
       b.nextSibling.remove(); // remove break
       b.remove(); // remove and
     }
@@ -191,4 +190,11 @@ endingControls.addEventListener("change", (event) => {
 // window resize
 window.addEventListener("resize", () => {
   scrollToBottom();
+});
+
+const aboutBtn = document.querySelector(".about-btn");
+const aboutDialog = document.querySelector(".about");
+console.log(aboutBtn, aboutDialog);
+aboutBtn.addEventListener("click", () => {
+  aboutDialog.showModal();
 });
